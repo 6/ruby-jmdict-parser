26f55f89a1881baa59d55e39779694f32cd99a52
Peter Graham <peter@1000memories.com>
2012-08-19 18:15:55 -0700

    Japanese word deinflector

--- main.rb
+++ main.rb
@@ -1,9 +1,13 @@
 #encoding: utf-8
 require './jmdict'
 
-radk = RadKParser.new("data/radkfilex")
+#radk = RadKParser.new("data/radkfilex")
 
 # test it out
-radk.radical_kanjis.each do |radical, kanjis|
-  p radical, kanjis
-end
+#radk.radical_kanjis.each do |radical, kanjis|
+#  p radical, kanjis
+#end
+
+jd = JapaneseDeinflector.new("data/deinflect.dat")
+puts jd.deinflect("歌った")
+puts jd.deinflect("嬉しくありません")
